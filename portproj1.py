99485b10d0eae14f7ffae5de9a87a0b066986676
Homero Ochoa <homeroochoa47@gmail.com>
2022-06-02 17:51:18 -0700

    init

---
+++ portproj1.py
@@ -0,0 +1,12 @@
+#Command Line prortfolio project
+
+#Welcome to the best friend of a modern youth: a tool that helps you figure out what film camera you should buy! what an exciting way to kick off your new hobby.
+#lets start with a few questions to figure out what's good for you. First off, what types of photos do you want to take? (Portraits, landscapes, pics of your friends, quick snapshots,) If you need some other ideas, enter "not sure"
+
+#input that takes in user response
+
+#Class of cameras that 
+
+
+
+#Class that defines our types of cameras
