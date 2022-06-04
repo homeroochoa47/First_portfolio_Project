27b407ff68ca4a4d6af0a63c0e547ce5751e04df
Homero Ochoa <homeroochoa47@gmail.com>
2022-06-03 23:27:31 -0700

    added another function for the second choice, as well as refining the first choice function

--- portproj1.py
+++ portproj1.py
@@ -4,31 +4,48 @@ print ("Welcome to the best friend of the modern youth: a tool that helps you fi
 
 print ("Lets start with a few questions to figure out the right choice for you.\n")
 
-#input that takes in user response
-#in the future: make sure user can only do one input at a time
-#first_input = input("First off, what types of photos do you want to take? (Portraits, landscapes, pics of your friends, quick snapshots: ")
-
-
-#providing options in case user inputs "not sure"
+#this function initates a couple of inputs and goes back to the start if it doesnt get an input we can use
 def first_choice():
-    first_input = input("What types of photos do you want to take? (Portraits, landscapes, pics of your friends, quick snapshots: ")
+    first_input = input("What types of photos do you want to take? (Portraits, landscapes, pics of your friends, quick snapshots: \n")
+
     if "portrait" in first_input.lower() or "landscape" in first_input.lower():
-        global second_input
-        second_input = input("Nice. Do you wand a camera with a fixed lens or do you want to be able to switch lenses?")
-        pass
-        #need to figure out how to sort through all of the cameras
-    elif "friends" in first_input.lower() or "snapshots" in first_input.lower():
-        second_input = int(input("You're probably best off with a point and shoot camera. What's your price range? "))
-        pass
+        global second_input #called global to reference in the next function
+        second_input = input("Nice. Do you wand a camera with a fixed lens or do you want to be able to switch lenses? \n")
+
+    elif "friends" in first_input.lower() or "snap" in first_input.lower():
+        try:
+             second_input = int(input("You're probably best off with a point and shoot camera. What's your price range? \n"))
+        except: 
+            print ("Please make sure to enter a numerical value")
+            return first_choice()
+        
     else: 
         print("Sorry, I didnt quite get that. Try again")
         return first_choice()
 
 first_choice()
 
-#called global in the last if statement in order to break up the conditionals into separate chunks. Didnt want one massive if statement to dictate the whole program
 
+def second_choice():
+    if type(second_input) == str:
+        global mech_or_batt_input
+        if "fixed" in second_input.lower():
+            mech_or_batt_input = input("Fixed lens it is. Now, are you interested in a fully mechanical camera, or one that relies on batteries? \n")
 
+        elif "switch" in second_input.lower() or "lenses" in second_input.lower():
+            mech_or_batt_input = input("So you'd prefer something with interchangeable lenses. Would you prefer battery powered or fully mechanical? \n")
+    elif type(second_input) == int:
+        if second_input > 0 and second_input < 50:
+            print("check out the pentax iqzoom")
+        elif second_input > 50 and second_input <= 150:
+            return #check out the olympus stylus
+        elif second_input > 150:
+            return # check out the konica big mini
+        elif second_input <= 0:
+            print ("Sorry, I didnt quite get that. Try again")
+            return first_choice()
+
+second_choice()
 
 
 
