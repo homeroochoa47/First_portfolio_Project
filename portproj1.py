f43bfa4ce7f668b6aa86fe610591d275402fadb5
Homero Ochoa <homeroochoa47@gmail.com>
2022-06-04 12:34:32 -0700

    created another test branch in order to restructure how the functions get called in order to fix a bug where the program was not looping after the first run-through.

--- portproj1.py
+++ portproj1.py
@@ -1,12 +1,12 @@
 #Command Line prortfolio project
 
+
 def restart_input():
     restart = input("Would you like to start over? \n")
     if "y" in restart.lower():
-        return first_choice()
+        return script()
     elif "n" in restart.lower():
         print ("See ya!")
-        exit()
     else:
         print ("Enter yes or no")
         restart_input()
@@ -16,16 +16,18 @@ def first_choice():
 
     #if the person chooses portrait or landscape
     if "portrait" in first_input.lower() or "landscape" in first_input.lower():
-        global second_input #called global to reference in the next function
+        #global second_input #called global to reference in the next function
         second_input = input("Nice. Do you wand a camera with a fixed lens or do you want to be able to switch lenses? \n")
+        return second_input
 
     #if they choose friends or snapshots
     elif "friends" in first_input.lower() or "snap" in first_input.lower():
         try:
              second_input = int(input("You're probably best off with a point and shoot camera. What's your budget? \n"))
+             return second_input
         except: 
             print ("Please make sure to enter a numerical value (without a $)")
-            return first_choice()
+            return script()
     
     elif "everything" in first_input.lower():
         print("Your best bet would be the Minolta Hi-Matic 7S.")
@@ -34,72 +36,79 @@ def first_choice():
     
     else: 
         print("Sorry, I didnt quite get that. Try again")
-        return first_choice()
+        return script()
 
 
-def second_choice():
-    # an input of portraits or landscapes results in a string
-    if type(second_input) == str:
-        global fixed_mech_or_batt_input
-        global inter_mech_or_batt_input
-        if "fixed" in second_input.lower():
-            fixed_mech_or_batt_input = input("Fixed lens it is. Now, are you interested in a fully mechanical camera, or one that relies on batteries? \n")
-   
-        elif "switch" in second_input.lower() or "lenses" in second_input.lower():
-            inter_mech_or_batt_input = input("So you'd prefer something with interchangeable lenses. Would you prefer battery powered or fully mechanical? \n")
-        
-
+def second_choice(choice):
     #choosing point and shoots above results in an int for price range
-    elif type(second_input) == int:
-        if second_input > 0 and second_input < 50:
+    if type(choice) == int:
+        if choice > 0 and choice < 50:
             print("Check out the Pentax IQZoom")
             iqzoom.more_info()
             restart_input()
-        elif second_input > 50 and second_input <= 150:
+        elif choice > 50 and choice <= 150:
             print ("Check out the Olympus Stylus Zoom")
             stylus.more_info()
             restart_input()
-        elif second_input > 150:
+        elif choice > 150:
             print ("check out the Konica Big Mini")
-            bigmini.moreinfo()
+            bigmini.more_info()
             restart_input()
-        elif second_input <= 0:
+        elif choice <= 0:
             print ("Sorry, I didnt quite get that. Try again")
-            return first_choice()
+            return script()
+   
+    # an input of portraits or landscapes results in a string
+    #depnding on this next choice, we either leave as a string or convert to a list and append the value
+    elif type(choice) == str:
+        if "fixed" in choice.lower():
+            fixed_mech_or_batt_input = input("Fixed lens it is. Now, are you interested in a fully mechanical camera, or one that relies on batteries? \n")
+            return fixed_mech_or_batt_input
+            #results in a string output
+   
+        elif "switch" in choice.lower() or "lenses" in choice.lower():
+            inter_mech_or_batt_input = input("So you'd prefer something with interchangeable lenses. Do you want battery powered or fully mechanical? \n")
+            inputlist = []
+            inputlist.append(inter_mech_or_batt_input)
+            return inputlist
+            #results in a list output, referenced in the "suggestion" function below
+        else:
+            print ("Sorry, I didnt quite get that. Try again")
+            return script()
 
 
-#if they chose a fixed lens for portraits/landscapes
-def fixed_suggestion():
-    if "mech" in fixed_mech_or_batt_input.lower():
-        print ("You might like the Olympus 35 RC")
-        olympus_rc.more_info()
-        return restart_input
-    elif "batt" in fixed_mech_or_batt_input.lower():
-        print ("You might like the Olympus XA")
-        olympus_xa.more_info()
-        return restart_input
-    else:
-        print("Whoops, lets try again. ")
-        first_choice()
+#if they chose portraits/landscapes
+#picks between a str if they chose fixed lens above, or a list if they chose interchangeable lenses
+def suggestion(choice):
+    if type(choice) == str: 
+        if "mech" in choice.lower():
+            print ("You might like the Olympus 35 RC")
+            olympus_rc.more_info()
+            restart_input()
+        elif "batt" in choice.lower():
+            print ("You might like the Olympus XA")
+            olympus_xa.more_info()
+            restart_input()
+        else:
+            print("Whoops, lets try again. ")
+            script()
 
-#if they chose interchangeable lens for portraits/landscapes
-def inter_suggestion():
-    if "mech" in inter_mech_or_batt_input.lower():
-        print ("How about the Nikon FM?")
-        nikon_fm.more_info()
-        return restart_input
-    elif "batt" in inter_mech_or_batt_input.lower():
-        print ("How about the Nikon FE?")
-        nikon_fe.more_info()
-        return restart_input
-    else:
-        print("Whoops, lets try again. ")
-        first_choice()
+    #if they chose interchangeable lens for portraits/landscapes
+    elif type(choice) == list:
+        if "mech" in choice[0].lower():
+            print ("How about the Nikon FM?")
+            nikon_fm.more_info()
+            restart_input()
+        elif "batt" in choice[0].lower():
+            print ("How about the Nikon FE?")
+            nikon_fe.more_info()
+            restart_input()
+        else:
+            print("Whoops, lets try again. ")
+            script()
 
 
-#function that asks if the user wants more info on their camera -- calls the repr in the camera class
-
-#class of cameras that identifies the attributes of the cameras
+#class of cameras with a couple of methods
 class Camera:
     
     #here we add some attributes for our camera, which will be referenced as we accept inputs
@@ -120,7 +129,7 @@ class Camera:
         if "y" in answer:
             print(self)
         elif "n" in answer:
-            print ("See ya!")
+            print ("Happy buying!")
         else:
             print ("Enter yes or no")
             restart_input()
@@ -144,16 +153,14 @@ nikon_fe = Camera("Nikon FE", 150, "interchangeable lens SLR", "battery powered"
 olympus_rc = Camera("Olympus 35RC", 100, "small fixed lens rangefinder","fully mechanical with optional batteries", "portraits and landscapes" )
 
 
-
-print ("Welcome to the best friend of the modern youth: a tool that helps you figure out what film camera you should buy! what an exciting way to kick off your new hobby.\n")
+#this is where we actully initiate the sequence of all of he functions, starting with two print statements.
+print ("\n Welcome to the best friend of the modern youth: a tool that helps you figure out what film camera you should buy! What an exciting way to kick off your new hobby.\n")
 
 print ("Lets start with a few questions to figure out the right choice for you.\n")
 
-first_choice()
 
-second_choice()
+#the functions were made to be called within oneanother, and the script function here is called to start the input sequence
+def script():
+    suggestion(second_choice(first_choice()))
 
-try:
-    fixed_suggestion()
-except:
-    inter_suggestion()
+script()
